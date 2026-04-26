13ad33702a42df44868f374814f783307aa6c142
Pehesara Banneheka <nishadapbanneheka@gmail.com>
2026-04-26 12:45:18 +0530

    0.00.00.00

--- obj/Debug/net8.0/OCVMS.AssemblyInfo.cs
+++ obj/Debug/net8.0/OCVMS.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OCVMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee447da078a7a003a769a37c7a92d96d80b7d8f")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14d34e5795bf3ec43c36cbba5df6390904afb07d")]
->>>>>>> Stashed changes
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+209c266e41da76b1e81dd3b584859dbc46b51d42")]
 [assembly: System.Reflection.AssemblyProductAttribute("OCVMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OCVMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
