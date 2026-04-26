4ea307986e6f65fd7a06c2c4bf4432add2f07c20
Pehesara Banneheka <nishadapbanneheka@gmail.com>
2026-04-26 14:29:19 +0530

    no

--- obj/Debug/net8.0/OCVMS.AssemblyInfo.cs
+++ obj/Debug/net8.0/OCVMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OCVMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16ce97daef9c1d9560b7f212b6bb3ee96b42b89e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef1e29b6d76c7068749a4fbac50e64b598791d4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OCVMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OCVMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
