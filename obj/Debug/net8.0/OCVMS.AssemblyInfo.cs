76763c8ee0bea5da494515df3be5bee39f84e25c
Sajith Priyashan Weerakkodi <sajithpriyashan99@gmail.com>
2026-04-26 19:55:14 +0530

    Dont panic

--- obj/Debug/net8.0/OCVMS.AssemblyInfo.cs
+++ obj/Debug/net8.0/OCVMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OCVMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07a1ecec8d0650fd5cdea994ccec9617d395842a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f76d47a1a67daf44df51b9fd5f8f698cc8768dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("OCVMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OCVMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
